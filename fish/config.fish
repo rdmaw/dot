b064c934c9e4c185cec93e31804ddfbca57dc5dd
redmaw <42783523+rdmaw@users.noreply.github.com>
2025-07-31 19:17:07 +0200

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_cursor_insert block
 
 zoxide init fish | source
 
 # overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+# function fish_greeting
+  # smth smth
+# end
 
 alias q=exit
 alias lg=lazygit
