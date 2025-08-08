62613bd42350cd8f21c85a90d030f126cac699dc
redmaw <42783523+rdmaw@users.noreply.github.com>
2025-08-08 22:47:19 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -g fish_key_bindings fish_vi_key_bindings
-set -g fish_cursor_insert block
+set -g fish_cursor_default block blink
+set -g fish_cursor_insert block blink
+set -g fish_cursor_replace underscore blink
+set -g fish_cursor_visual block blink
 
 zoxide init fish | source
 
